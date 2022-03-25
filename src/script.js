06339728bb93fc091469fbe489e2cfce3feeb9d7
bbeltran22 <99772609+bbeltran22@users.noreply.github.com>
2022-03-25 12:22:17 -0600

    Fixed F->C

--- src/script.js
+++ src/script.js
@@ -20,15 +20,15 @@ if (minutes < 10) {
 let currentDate = document.querySelector("#currentDate");
 currentDate.innerHTML = `${day} ${hours}:${minutes}`;
 
-function showTemperature(response) {
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let iconElement = document.querySelector("#icon");
   let tempMaxElement = document.querySelector("#temp-max");
   let tempMinElement = document.querySelector("#temp-min");
   let feelsLikeElement = document.querySelector("#feels-like");
-  let iconElement = document.querySelector("#icon");
 
   fahrenheitTemperature = response.data.main.temp;
 
@@ -45,12 +45,13 @@ function showTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
 function search(city) {
   let apiKey = "2fc2d2f29940c2aa3c6f40c54ba37b97";
   let units = "imperial";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiSearchUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiSearchUrl).then(showTemperature);
+  axios.get(apiSearchUrl).then(displayTemperature);
 }
 
 function searchCity(event) {
@@ -60,17 +61,16 @@ function searchCity(event) {
   search(city);
 }
 
-function showCelsiusTemperature(event) {
+function displayCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   fahrenheitLink.classList.remove("active");
   celsiusLink.classList.add("active");
   let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
-
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-function showFahrenheitTemperature(event) {
+function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   fahrenheitLink.classList.add("active");
@@ -78,13 +78,13 @@ function showFahrenheitTemperature(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
+let fahrenheitTemperature = null;
+
 let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", showCelsiusTemperature);
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
-
-let fahrenheitTemperature = null;
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCity);
